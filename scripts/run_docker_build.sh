4a6db5af3a51c07356075b7759e5256681ed777c
Ming Yao <yaoming.thu@gmail.com>
2018-06-15 14:45:35 -0400

    add travis

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker pull quay.io/pypa/manylinux1_x86_64:latest
+
 DOCKER_IMAGE="quay.io/pypa/manylinux1_x86_64"
 
 docker run -v `pwd`:/code $DOCKER_IMAGE /code/scripts/docker_build.sh

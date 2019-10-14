2cd96fb79187897279e256c87bad0663c7e15aca
Ming Yao <yaoming.thu@gmail.com>
2019-10-14 19:11:25 +0800

    try fix

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -9,19 +9,19 @@ for PYBIN in /opt/python/*/bin; do
     if [[ "$PYBIN" =~ (cp27|cp34|cp35|cp36|cp37) ]]; then
         ${PYBIN}/pip install -r /code/dev-requirements.txt
         ${PYBIN}/pip install -r /code/test-requirements.txt
-        ${PYBIN}/pip wheel /code/ -w dist/
+        ${PYBIN}/pip wheel /code/ -w wheelhouse/
     fi
 done
 
 # Bundle external shared libraries into the wheels
-for whl in dist/*.whl; do
-    auditwheel repair $whl --plat $PLAT -w /code/dist/
+for whl in wheelhouse/*.whl; do
+    auditwheel repair $whl --plat $PLAT -w /code/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     if [[ "$PYBIN" =~ (cp27|cp34|cp35|cp36|cp37) ]]; then
-        ${PYBIN}/pip install pysbrl --no-index -f /code/dist
+        ${PYBIN}/pip install pysbrl --no-index -f /code/wheelhouse
         (cd /code; ${PYBIN}/python -m pytest)
     fi
 done
